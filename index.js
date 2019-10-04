664c03acc9664efe84c53237cef5b52a180178f9
Tamim Khan <tzkhan10@gmail.com>
2019-10-04 23:05:55 +0100

    Add logic to prefix both title and body

--- index.js
+++ index.js
@@ -7,18 +7,17 @@ async function run() {
       branch: '%branch%'
     }
 
-    const token = core.getInput('repo-token', {required: true});
-
-    const branchRegex = core.getInput('branch-regex', {required: true});
-    core.debug(`branchRegex: ${branchRegex}`);
-
-    const prefixTemplate = core.getInput('prefix-template', {required: true});
-    core.debug(`prefixTemplate: ${prefixTemplate}`);
+    const inputs = {
+      token: core.getInput('repo-token', {required: true}),
+      branchRegex: core.getInput('branch-regex', {required: true}),
+      titleTemplate: core.getInput('title-template', {required: true}),
+      bodyTemplate: core.getInput('body-template', {required: true}),
+    }
 
     const branch = github.context.payload.pull_request.head.ref.toLowerCase();
     core.debug(`branch: ${branch}`);
 
-    const matches = branch.match(new RegExp(branchRegex));
+    const matches = branch.match(new RegExp(inputs.branchRegex));
     if (!matches) {
       core.setFailed('Branch name does not match given regex');
       return;
@@ -26,27 +25,44 @@ async function run() {
 
     core.info(`Matched branch text: ${matches[0]}`);
 
-    const prefix = prefixTemplate.replace(textTokens.branch, matches[0]);
-    core.debug(`prefix: ${prefix}`);
-
-    const title = github.context.payload.pull_request.title;
-    core.debug(`title: ${title}`);
-
-    if (title.toLowerCase().startsWith(prefix.toLowerCase())) {
-      core.warning('PR title is prefixed correctly already - no updates made');
-      return;
-    }
-
-    const newTitle = prefix.toUpperCase().concat(' ', title);
-    core.debug(`newTitle: ${newTitle}`);
-
-    const client = new github.GitHub(token);
-    const response = await client.pulls.update({
+    const request = {
       owner: github.context.repo.owner,
       repo: github.context.repo.repo,
       pull_number: github.context.payload.pull_request.number,
-      title: newTitle,
-    });
+    }
+
+    const titlePrefix = inputs.titleTemplate.replace(textTokens.branch, matches[0].toUpperCase());
+    core.debug(`titlePrefix: ${titlePrefix}`);
+
+    const title = github.context.payload.pull_request.title;
+    const updateTitle = !title.toLowerCase().startsWith(titlePrefix.toLowerCase());
+
+    if (updateTitle) {
+      request.title = titlePrefix.concat(' ', title);
+      core.debug(`new title: ${request.title}`);
+    } else {
+      core.warning('PR title is prefixed already - no updates made');
+    }
+
+    const bodyPrefix = inputs.bodyTemplate.replace(textTokens.branch, matches[0].toUpperCase());
+    core.debug(`bodyPrefix: ${bodyPrefix}`);
+
+    const body = github.context.payload.pull_request.body;
+    const updateBody = !body.toLowerCase().startsWith(bodyPrefix.toLowerCase());
+
+    if (updateBody) {
+      request.body = bodyPrefix.concat('\n\n', body);
+      core.debug(`new body: ${request.body}`);
+    } else {
+      core.warning('PR body is prefixed already - no updates made');
+    }
+
+    if (!updateTitle && !updateBody) {
+      return;
+    }
+
+    const client = new github.GitHub(inputs.token);
+    const response = await client.pulls.update(request);
 
     core.info(`response: ${response.status}`);
     if (response.status !== 200) {
