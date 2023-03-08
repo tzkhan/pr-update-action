18db16de8a84f63c4bdf74526bc308bf22292d2e
Jamie Wright <jwright3478@gmail.com>
2023-03-08 17:04:11 +0000

    const to var

--- index.js
+++ index.js
@@ -83,7 +83,7 @@ async function run() {
       core.setOutput("headMatch", matches.headMatch);
     }
 
-    const request = {
+    var request = {
       owner: github.context.repo.owner,
       repo: github.context.repo.repo,
       pull_number: github.context.payload.pull_request.number,
